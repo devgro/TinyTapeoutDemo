86be43c43540df36a625021d88c99edff25a9f05
devgro <devan.grover@gmail.com>
2023-10-17 20:18:33 -0500

    Update calculator.sv

--- src/calculator.sv
+++ src/calculator.sv
@@ -26,9 +26,14 @@ module tt_um_calculator_chip (
     input logic [7:0] NumIn,
     input logic [1:0] OpIn,
     input logic Enter,
-    input logic rst_n,
-    input logic ena,
-    input logic clk);
+    input  logic [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output logic [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  logic [7:0] uio_in,   // IOs: Bidirectional Input path
+    output logic [7:0] uio_out,  // IOs: Bidirectional Output path
+    output logic [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  logic       ena,      // will go high when the design is enabled
+    input  logic       clk,      // clock
+    input  logic       rst_n     // reset_n - low to reset);
 
     logic [7:0] state_in;
 
